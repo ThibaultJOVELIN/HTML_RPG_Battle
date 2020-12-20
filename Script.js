dd6170f88dc2c94618d81411f9d6f8527dc8a89d
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-20 17:03:43 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,32 @@
-document.getElementById("Appearance")
+var PV_Monster1
+var PV_Monster2
+var PV_Monster3
+
+var Atk_Monster1
+var Atk_Monster2
+var Atk_Monster3
+
+
+var PV_BloodMage
+var PV_Alchemsit
+var PV_Paladin
+
+var Atk_BloodMage
+var Atk_Alchemsit
+var Atk_Paladin
+
+var CostSpell_BloodMage
+var CostSpell_Alchemist
+var CostSpell_Paladin
+
+var CostDefence_BloodMage
+var CostDefence_Alchemist
+var CostDefence_Paladin
+
+var ValueDefence_BloodMage
+var ValueDefence_Alchemist
+var VakueDefence_Paladin
+
+var Atk_Spell_BloodMage
+var Atk_Spell_Alchemist
+var Heal_Spell_Paladin
