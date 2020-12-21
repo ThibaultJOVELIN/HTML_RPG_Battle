6d25e079589358e31ece2f3adccec95686d689eb
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-21 15:11:18 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -31,3 +31,11 @@ var Atk_Spell_BloodMage
 var Atk_Spell_Alchemist
 var Heal_Spell_Paladin
 
+function disappearance() {
+var A = document.getElementById('Bubble');
+something.style.opacity = "0.5";
+}
+
+function hover() {
+var B = document.getElementById('Bubble');
+something.style.opacity = "1";
