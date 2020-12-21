d62a8bd95136d1d24c0ed1449a2f5b03517acf96
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-21 15:54:32 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,38 +1,38 @@
-var PV_Monster1
-var PV_Monster2
-var PV_Monster3
+var PV_Monster1 = 100
+var PV_Monster2 = 150
+var PV_Monster3 = 100
 
-var Atk_Monster1
-var Atk_Monster2
-var Atk_Monster3
+var Atk_Monster1 = 30
+var Atk_Monster2 = 40
+var Atk_Monster3 = 30
 
-var PV_BloodMage
-var PV_Alchemsit
-var PV_Paladin
+var PV_BloodMage = 200
+var PV_Alchemsit = 190
+var PV_Paladin = 260
 
-var Atk_BloodMage
-var Atk_Alchemsit
-var Atk_Paladin
+var Atk_BloodMage = 30
+var Atk_Alchemsit = 40
+var Atk_Paladin = 35
 
-var Defence_BloodMage
-var Defence_Alchemist
-var Defence_Paladin
+var Defence_BloodMage = 0
+var Defence_Alchemist = 0
+var Defence_Paladin = 10
 
-var CostSpell_BloodMage
-var CostSpell_Alchemist
-var CostSpell_Paladin
+var CostSpell_BloodMage = 30
+var CostSpell_Alchemist = 20
+var CostSpell_Paladin = 20
 
-var CostDefence_BloodMage
-var CostDefence_Alchemist
-var CostDefence_Paladin
+var CostDefence_BloodMage = 20
+var CostDefence_Alchemist = 20
+var CostDefence_Paladin = 10
 
-var ValueDefence_BloodMage
-var ValueDefence_Alchemist
-var VakueDefence_Paladin
+var ValueDefence_BloodMage = 20
+var ValueDefence_Alchemist = 20
+var ValueDefence_Paladin = 45
 
-var Atk_Spell_BloodMage
-var Atk_Spell_Alchemist
-var Heal_Spell_Paladin
+var Atk_Spell_BloodMage = 80
+var Atk_Spell_Alchemist = 50
+var Heal_Spell_Paladin = 80
 
 function invisible() {
 var A = document.getElementById('Bubble');
